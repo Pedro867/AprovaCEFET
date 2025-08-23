6fa45e02f5f627daa741a50e36767ecfaf2971d3
Rafael Torres <136863992+T0RR35@users.noreply.github.com>
2025-08-23 19:39:46 -0300

    Delete backend directory (ler descrição)
    Como o backend está hospedado no render, não há necessidade de ficar aqui. Se quiserem saber como está, entrem no meu repositório no git (já compartilhei com vcs)

--- backend/validacaoCadastro.js
+++
@@ -1,8 +0,0 @@
-/*export function validaCadastro(nome, email, senha){
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(emailRegex.test(email)){
-        return "email inválido!";
-    }
-}
-
-module.exports = { validaCadastro };*/
